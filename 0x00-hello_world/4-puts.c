327bccf19cbebb19684a309ceaf585c0cc0767fc
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-03 17:32:01 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)/*no desciption found*/
+/*nooo*/int/*no descrit*/ main(void)/*no desciption found*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
