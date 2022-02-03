c8b07e842fe203ca141e93ac15a104632a651c9b
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-03 19:22:31 +0100

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
-/*nooo*/int/*no descrit*/ main(void)/*no desciption found*/
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
 {
-printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+printf("Size of a char: 1 byte(s)\n");
+printf("Size of an int: 4 byte(s)\n");
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: 8 byte(s)\n");
+printf("Size of a float: 4 byte(s)\n");
 return (0);
 }
