6a98f7a422b3249f9a1d0eef193dbf9a9ff92561
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-03 18:43:38 +0100

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /*nooo*/int/*no descrit*/ main(void)/*no desciption found*/
 {
-printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\n
-Size of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
+printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\n")
+printf("Size of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)\n");
 return (0);
 }
