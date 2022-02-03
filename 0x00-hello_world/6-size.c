4653d3a980f1b468e9bd205fa1d549412e182e1f
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-03 18:10:47 +0100

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,6 @@
 
 /*nooo*/int/*no descrit*/ main(void)/*no desciption found*/
 {
-gcc 6-size.c -m32 -o size32 2> /tmp/32
-gcc 6-size.c -m64 -o size64 2> /tmp/64
+printf("gcc 6-size.c -m32 -o size32 2> /tmp/32\ngcc 6-size.c -m64 -o size64 2>/tmp/64")
 return (0);
 }
