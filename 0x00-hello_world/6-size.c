f5dfd2beb4bacaa9ed97f0f44446069c393af0dd
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-03 18:18:07 +0100

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,6 @@
 
 /*nooo*/int/*no descrit*/ main(void)/*no desciption found*/
 {
-printf("gcc 6-size.c -m32 -o size32 2> /tmp/32\ngcc 6-size.c -m64 -o size64 2>/tmp/64")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
