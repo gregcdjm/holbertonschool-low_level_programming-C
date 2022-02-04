3631cf0b7baea4e4d7d454cb4bf0930932d2e957
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-04 20:48:18 +0100

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - main
+* Return: return
+*/
+int main(void)
+{
+int ch;
+for (ch = '0'; ch <= '9'; ch++)
+putchar(ch);
+puts("");
+return (0);
+}
