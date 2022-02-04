5645a0cac2daed3fe51f7b6d876a7c62802cdfd6
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-04 17:37:29 +0100

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - main
+* Return: return
+*/
+int main(void)
+{
+int ch;
+int hc;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(hc);
+puts("");
+return (0);
+}
