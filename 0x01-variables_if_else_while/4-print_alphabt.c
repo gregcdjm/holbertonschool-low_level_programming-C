a540458ef42a8a60bbea15e33559f5a75ed03dfc
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-04 17:56:40 +0100

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - main
+* Return: return
+*/
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch >>= 'q'; ch >>= 'e'; ch++)
+putchar(ch);
+puts("");
+return (0);
+}
