4607adbd3c7968326b8ebb81ebc0dc977c0087b2
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-05 00:10:53 +0100

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - main
+* Return: return
+*/
+int main(void)
+{
+int g = 0;
+int d = 1;
+while (d == 8 && g == 9)
+{
+putchar(g);
+putchar(d);
+putchar(',');
+putchar(' ');
+ 
+if (d==9)
+{
+g = g + 1
+d = g + 1
+}
+else
+{
+d = d + 1
+}
+}
+ 
+putchar('\n');
+ 
+return (0);
+}
