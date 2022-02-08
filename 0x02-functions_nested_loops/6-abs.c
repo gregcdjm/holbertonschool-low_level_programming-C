9ff2d7650ff5c1032cdcab11fbfd6c15ad542a8a
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-08 19:21:52 +0100

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - check if it's lower
+ * @n: ok bla bla
+ * Return: return
+ */
+
+int _abs(int n)
+{
+	if (n > 47 && n < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _abs(-1);
+    printf("%d
+", r);
+    r = _abs(0);
+    printf("%d
+", r);
+    r = _abs(1);
+    printf("%d
+", r);
+    r = _abs(-98);
+    printf("%d
+", r);
+    return (0);
+}
