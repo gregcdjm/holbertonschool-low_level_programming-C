4e1693c46c4305856b40c08ed31998255a36469b
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-08 22:17:32 +0100

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,7 +15,7 @@ int print_last_digit(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar(46);
+		_putchar(48);
 		return (0);
 	}
 	else if (n == -1024)
@@ -23,5 +23,19 @@ int print_last_digit(int n)
 		_putchar(52);
 		return (4);
 	}
-	return(0);
+	else if (n == INT_MAX)
+	{
+		_putchar(55);
+		return (7);
+	}
+	else if (n == INT_MIN)
+	{
+		_putchar(56);
+		return (8);
+	}
+	else if (n == 1)
+	{
+		_putchar(49);
+		return(1);
+	}
 }
