c54b96682cf7353e84cb289d6d43b2c4277447ed
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-09 20:36:18 +0100

    10

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,15 +24,16 @@ void print_to_98(int n)
 			}
 		}
 	}
-	putchar('\n');
 	if (n > 98)
 	{
 		for (n = n; n >= 98; n--)
 		{
 			printf("%d",n);
 			if (n != 98)
+			{
 				printf(", ");
+			}
 		}
 	}
-
+	putchar('\n');
 }
