23c3d156638cb32297b40348faeafcecf0d1741b
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-09 20:23:37 +0100

    10

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - check if it's lower
+ * @n: 1st ereg
+ * Return: return
+ */
+
+void print_to_98(int n)
+{
+	if (n == 98)
+	{
+		printf("98");
+	}
+	if (n < 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			printf("%d",n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	if (n > 98)
+	{
+		for (n = n; n >= 98; n--)
+		{
+			printf("%d",n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+
+}
