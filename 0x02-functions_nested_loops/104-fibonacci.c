d7c937d751f3434155513e1ebb75b15262ca7b35
gregcdjm <grgoirecoudrin@gmail.com>
2022-02-10 21:24:47 +0100

    0

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - main
+ *
  * @Fn: Toujours égal à F0+F1
  * @F0: Toujours égal à
  * @F1: Toujours
  * @C: Toujours
+ *
  * Return: return
  */
 
@@ -17,14 +18,28 @@ int main(void)
 	unsigned long Fn;
 	int C = 0;
 
-	printf("%lu, ",F0);
-        printf("%lu, ",F1);
+	printf("%lu, ", F0);
+	printf("%lu, ", F1);
 	while (C <= 89)
 	{
+		1Fxx00 = F1 % 100;
+		1F11xx = F1 / 100;
+		0Fxx00 = F0 % 100;
+		0F11xx = F0 / 100;
+		1F0Fxx00 = 1Fxx00 + 0Fxx00;
+		1F0F00xx = 1F00xx + 0F00xx;
+		if (1F0Fxx00 > 100)
+		{
+			1F0F00xx = 1F0F00xx + 1;
+			1F0Fxx00 = 1F0Fxx00 - 100;
+		}
+		FN = 1F0F00xx + 1F0Fxx00;
+		F0 = 0Fxx00 = 
+		
 		Fn = F1 + F0;
 		F0 = F1;
 		F1 = Fn;
-		printf("%lu, ",Fn);
+		printf("%lu, ", Fn);
 		C++;
 	}
 	add_2();
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+void postitive_or_negative(int i);
