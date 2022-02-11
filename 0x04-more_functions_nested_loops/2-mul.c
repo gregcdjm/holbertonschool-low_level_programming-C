02cf81c089ad8dbbbbd7ce039f92f2480c32070a
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-11 15:49:08 +0100

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* int mul(int a, int b) - writes the character c to stdout
+* mul - writes the character c to stdout
 * @a: The character to print
 * @b: caca
 * Return: On success 1.
