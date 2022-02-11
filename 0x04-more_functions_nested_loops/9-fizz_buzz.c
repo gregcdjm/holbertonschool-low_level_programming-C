3fdc5ef0ff3cb9127ced858740f6621d1fa10bb1
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-11 22:26:44 +0100

    Ç

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int A = 1;
 while (A < 100)
 {
 if (A / (3 * 5) == 0)
-printf("FizzBuzz");
-else if (A / 3 == 0);
-printf("Fizz");
-else if (A / 5 == 0);
-printf("Buzz");
+printf("FizzBuzz ");
+else if (A / 3 == 0)
+printf("Fizz ");
+else if (A / 5 == 0)
+printf("Buzz ");
 else
 {
-printf("%d" , A)
+printf("%d " , A);
 }
-A++
+A++;
 }
 }
