da2d0e73c573d286707490b2df26e002ffebabf0
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-11 21:13:29 +0100

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,11 +1,27 @@
 #include "main.h"
 
 /**
-* print_diagonal - writes the character c to stdout
+ * print_diagonal - writes the character c to stdout
 * @n: vava
 * Return: On success 1.
 */
 
 void print_diagonal(int n)
 {
-  for (n = n
+int b = -1, a;
+
+if (n > 0)
+{
+while (b + 1 > -n)
+{
+for (a = b + n; a < n; a++)
+{
+_putchar('.');
+}
+_putchar('\\');
+_putchar('\n');
+b--;
+}
+}
+_putchar('\n');
+}
