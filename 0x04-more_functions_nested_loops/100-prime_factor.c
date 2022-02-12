38eba8f590ae5aeba9d656a03213b1c272b25ae1
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-12 15:14:53 +0100

    12

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 /**
 * main - writes the character c to stdout
@@ -9,12 +8,10 @@
 int main(void)
 {
 long int  A, D, B;
-float s;
 
 A = 612852475143;
 D = 2;
-s = sqrt(612852475143);
-while (D < s)
+while (D < 782849)
 {
 if ((A % D) <= 0)
 {
