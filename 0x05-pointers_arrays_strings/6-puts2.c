f90d9c45a53a15fb038421c75e694a5eca80a12a
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-14 17:14:02 +0100

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ * @str: string
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int a = 0, b = 0;
+
+	while (s[a] != 0)
+		a++;
+	while (b < a)
+	{
+		_putchar(str[b]);
+		b++;
+		b++
+	}
+}
