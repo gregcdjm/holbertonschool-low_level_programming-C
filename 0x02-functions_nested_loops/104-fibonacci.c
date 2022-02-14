35706c3996375cbce8869b38ccced929048000da
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-14 02:48:04 +0100

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,8 @@ int main(void)
 {
 	unsigned long C = 0, Axx00, A00xx, Bxx00, B00xx, Cxx00, C00xx;
 
-	Axx00 = 2, A00xx = 0, Bxx00 = 1, B00xx = 0, printf("1, 2, ");
+	Axx00 = 2, A00xx = 0, Bxx00 = 1, B00xx = 0;
+	printf("%lu, %lu, ", Bxx00, Axx00);
 	while (C <= 95)
 	{
 		Cxx00 = Axx00 + Bxx00, C00xx = A00xx + B00xx;
@@ -18,7 +19,7 @@ int main(void)
 		if (C < 13)
 			printf("%lu, ", Cxx00);
 		else if (Cxx00 < 100 && C <= 94)
-			printf("%lu%lu%lu, ", C00xx, C*0, Cxx00);
+			printf("%lu%lu%lu, ", C00xx, C * 0, Cxx00);
 		else if (C <= 94)
 			printf("%lu%lu, ", C00xx, Cxx00);
 		Bxx00 = Axx00, B00xx = A00xx, A00xx = C00xx;
