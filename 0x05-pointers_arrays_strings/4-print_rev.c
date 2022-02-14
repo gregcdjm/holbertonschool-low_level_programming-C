3475d2a98ed7ff887564ffdebc18ccf0f8f8a6ba
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-14 14:49:07 +0100

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: s
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != 0)
+		a++;
+	while (a > -1)
+	{
+		_putchar(s[a]);
+		a--;
+	}
+	_putchar('\n');
+}
