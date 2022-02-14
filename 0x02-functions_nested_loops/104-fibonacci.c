b40794e170b4e852a97df6087e1a63b868f680cb
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-14 02:00:42 +0100

    12

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,26 +13,22 @@ int main(void)
 	printf("1, 2, ");
 	while (C <= 95)
 	{
-		Cxx00 = Axx00 + Bxx00;
-		C00xx = A00xx + B00xx;
-		if (Cxx00 > 1000)/*Here we add 1 to the right group numbers*/
-		{/*               if the left group numbers > 1000*/
-			C00xx = C00xx + 1;
-			Cxx00 = Cxx00 - 1000;
-		}
+		Cxx00 = Axx00 + Bxx00 , C00xx = A00xx + B00xx;
+		if (Cxx00 > 1000)
+			C00xx = C00xx + 1, Cxx00 = Cxx00 - 1000;
 		if (C < 13)
 			printf("%lu, ", Cxx00);
 		else if (Cxx00 < 100 && C <= 94)
-			printf("%lu0%lu, ", C00xx, Cxx00);
+		{
+			printf("%lu", C00xx), putchar(48);
+			printf("%lu, ", Cxx00);
+		}
 		else if (C <= 94)
 		{
 			printf("%lu%lu, ", C00xx, Cxx00);
-		} /*             above we print Fn(+1)*/
-		Bxx00 = Axx00;/*Here we do Fn(-1) = Fn*/
-		B00xx = A00xx;
-		A00xx = C00xx;/*Here we do Fn = Fn(+1)*/
-		Axx00 = Cxx00;
-		C++;
+		}
+		Bxx00 = Axx00, B00xx = A00xx, A00xx = C00xx;
+		Axx00 = Cxx00, C++;
 	}
 	printf("%lu%lu \n", C00xx, Cxx00);
 	return (0);
