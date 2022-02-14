05c62e2376f0347d13b13c7dfa163e92592a80e8
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-14 17:45:04 +0100

    6

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ * @str: string
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int a = 0, b = 0;
+
+	while (str[a] != 0)
+		a++;
+	a = a - 1;
+	b = a
+	if (a % 2 == 0)
+		for (a / 2; a < b; a++)
+		{
+			_putchar(str[a]);
+		}
+	else
+	{
+		for (a + 1 / 2; a < b; a++)
+		{
+			_putchar(str[a]);
+		}
+	}
+	_putchar('\n');
+}
