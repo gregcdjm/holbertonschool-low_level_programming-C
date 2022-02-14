9ba230c82a2f82cfbd06f69ee047a5a6f588cbbe
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-14 17:18:34 +0100

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,12 +9,13 @@ void puts2(char *str)
 {
 	int a = 0, b = 0;
 
-	while (s[a] != 0)
+	while (str[a] != 0)
 		a++;
 	while (b < a)
 	{
 		_putchar(str[b]);
 		b++;
-		b++
+		b++;
 	}
+	_putchar('\n');
 }
