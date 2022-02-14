f52dde74bc1e7bed2f6e4cf9935a08c79deb4cbd
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-14 02:11:42 +0100

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,8 +9,7 @@ int main(void)
 {
 	unsigned long C = 0, Axx00, A00xx, Bxx00, B00xx, Cxx00, C00xx;
 
-	Axx00 = 2, A00xx = 0, Bxx00 = 1, B00xx = 0;
-	printf("1, 2, ");
+	Axx00 = 2, A00xx = 0, Bxx00 = 1, B00xx = 0, printf("1, 2, ");
 	while (C <= 95)
 	{
 		Cxx00 = Axx00 + Bxx00, C00xx = A00xx + B00xx;
