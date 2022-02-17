7782f3b6b7676be0ad932897f42676a2e369697c
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-17 12:41:39 +0100

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int b = 0;
+	char c;
+
+	while (b < n / 2)
+	{
+		c = a[b];
+		a[b] = a[n - b - 1];
+		a[n - b - 1] = c;
+		b++;
+	}
+}
