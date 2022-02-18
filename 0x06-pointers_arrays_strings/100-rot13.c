9eb3a2662a285791d45b19f8862fcbd0047cc798
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-18 12:16:16 +0100

    100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* rot13 - ok
+* @str: oka
+*
+* Return: str
+*/
+
+char *rot13(char *str)
+{
+	int i = 0;
+	int f;
+
+	while (i < 99 )
+	{
+		f = 0;
+		while (((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123)) && (f < 1))
+		{
+			str[i] -= 13;
+			if((str[i] < 65 && str[i] > 51) || (str[i] > 83 && str[i] < 97) )
+			{
+				str[i] += 26;
+			}
+			f = 3;
+		}
+		i++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *);
 int _putchar(char c);
+char *rot13(char *);
