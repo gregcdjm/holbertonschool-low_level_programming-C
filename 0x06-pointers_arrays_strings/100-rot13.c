13a83b2be9c28add0a8567d9b02326ab90404461
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-18 12:19:48 +0100

    103

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,13 +12,15 @@ char *rot13(char *str)
 	int i = 0;
 	int f;
 
-	while (i < 99 )
+	while (i < 99)
 	{
 		f = 0;
-		while (((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123)) && (f < 1))
+		while (((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] <
+							 123)) && (f < 1))
 		{
 			str[i] -= 13;
-			if((str[i] < 65 && str[i] > 51) || (str[i] > 83 && str[i] < 97) )
+			if ((str[i] < 65 && str[i] > 51) || (str[i] > 83 && str[i]
+							    < 97))
 			{
 				str[i] += 26;
 			}
