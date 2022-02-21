f46fab24aa7de037cc136a8f3dfac0296ca2d0fd
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-21 17:29:25 +0100

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,14 +8,14 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int a = 0, b;
+	int a = 0, b = 0;
 	char *c;
 
-	for (a = 0; accept[a] != 0; a++)
+	for (a = 0; s[a] != 0; a++)
 	{
 		c = &s[a];
-		for (b = 0; s[b] != 0 && s[b] != ','; b++)
-			if (s[b] == accept[a])
+		for (b = 0; accept[b] != 0; b++)
+			if (s[a] == accept[b])
 				return (c);
 	}
 	return ('\0');
