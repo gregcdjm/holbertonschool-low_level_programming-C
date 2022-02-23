9e96d3707571b9e8921554404852d30e9df73757
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-23 12:58:54 +0100

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: string
+ * Return: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s++)
+		_putchar(*s), _puts_recursion(s);
+}
