33425e1bd6adc29367adc2d00acb05c1d6b88023
gregcdjm <gregoirecoudrin@gmail.com>
2022-02-25 13:53:55 +0100

    2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,9 +5,11 @@
  * @argv: argu string
  * Return: succes 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void)*argv;
-	printf("%d\n", argc);
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
