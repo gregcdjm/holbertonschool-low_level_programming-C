f7ae24dc3ab0da52b0a495d33731d25fba86994f
gregcdjm <gregoirecoudrin@gmail.com>
2022-03-01 13:42:25 +0100

    à

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - writes the character c to stdout
+* Return: On success 1.
+*/
+
+int main(void)
+{
+long int  A, D;
+
+A = 612852475143;
+D = 2;
+while (A > D)
+{
+if ((A % D) == 0)
+A = (A / D);
+else
+{
+D++;
+}
+}
+printf("%ld\n", A);
+return (0);
+}
