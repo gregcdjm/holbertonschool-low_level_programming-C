6fbee222d5315c308c865adfa0d5e1e6c52e0cea
gregcdjm <gregoirecoudrin@gmail.com>
2022-03-02 13:22:07 +0100

    0

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _atoi - function
+ * @s: param
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+int i, j, n, x;
+
+	i = n = 0;
+	x = 1;
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			x *= -1;
+		i++;
+	}
+	j = i;
+	while ((s[j] >= '0') && (s[j] <= '9'))
+	{
+		n = (n * 10) + x * ((s[j]) - '0');
+		j++;
+	}
+	return (n);
+}
