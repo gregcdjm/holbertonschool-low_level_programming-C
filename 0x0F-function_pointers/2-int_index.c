c8c24cc9663ae24854b20104c7d48da8a8bb871b
gregcdjm <gregoirecoudrin@gmail.com>
2022-03-09 14:50:22 +0100

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - prints an integer
+ * @array: the integer to print
+ * @size: j
+ * @cmp: l
+ * Return: int.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array && !size && !cmp && (size <= 0))
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
