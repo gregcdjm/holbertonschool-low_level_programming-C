d5ef2db59485784933cbef5af9fa260b5fe70d6f
gregcdjm <gregoirecoudrin@gmail.com>
2022-03-17 16:04:10 +0100

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - print element of structure list
+ * @h: what to print (with .str .len)
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		i++;
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+	}
+	return (i);
+}
