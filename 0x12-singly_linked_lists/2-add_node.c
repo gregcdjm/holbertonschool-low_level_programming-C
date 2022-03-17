9a156659cfd1541279d015b172dafabf06da2027
gregcdjm <gregoirecoudrin@gmail.com>
2022-03-17 17:32:41 +0100

    e

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,22 +2,26 @@
 #include <string.h>
 
 /**
- * add_note - print element of structure list
- * @h: what to print (with .str .len)
- * str: new string
+ * add_node - print element of structure list
+ * @head: what to print (with .str .len)
+ * @str: new string
  * Return: the number of nodes
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *s;
+	unsigned int i = 0;
+
+	for (; str[i]; i++)
+		;
 
 	s = malloc(sizeof(list_t));
 	if (s == NULL)
-		return(0);
+		return (0);
 
 	s->str = strdup(str);
-	s->len = strlen(str);
+	s->len = i;
 	s->next = *head;
 
 	*head = s;
