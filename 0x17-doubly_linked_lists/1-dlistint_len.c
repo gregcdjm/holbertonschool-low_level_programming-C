7c84d7d2aa84e459c3d66a40945362e38c8df017
gregcdjm <gregoirecoudrin@gmail.com>
2022-05-05 13:15:09 +0200

    Task1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,17 @@
-ok
+#include "lists.h"
+/**
+ * dlistint_len - fonction that count the size of the Dlist
+ * @h: List to count
+ * Return: Number of node
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
