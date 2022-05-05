2330575e3065a3f2d034f3ae6a6a3360cee03067
gregcdjm <gregoirecoudrin@gmail.com>
2022-05-05 14:22:28 +0200

    Task6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,21 @@
-ok
+#include "lists.h"
+/**
+ * sum_dlistint - fonction that return a node at index
+ * @head: List
+ * Return: add
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = head;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+		i += tmp->n;
+	}
+	return (i);
+}
