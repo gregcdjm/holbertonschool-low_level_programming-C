07fb39edd052c4b6a039bd0576214d04dbbc9bd2
gregcdjm <gregoirecoudrin@gmail.com>
2022-05-06 14:37:53 +0200

    task8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -52,7 +52,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	tmp->prev->next = tmp->next;
 	if (tmp->next)
 	{
-		tmp->next = tmp->prev;
+		tmp->prev = tmp->next;
 	}
 	free(tmp);
 	return (1);
