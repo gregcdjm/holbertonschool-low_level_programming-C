4a7b1dbe304dc0797514b1d7be61d3c92022e8e4
gregcdjm <gregoirecoudrin@gmail.com>
2022-05-06 14:23:51 +0200

    task8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -42,6 +42,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			(*head)->prev->next = NULL;
 			(*head)->prev = NULL;
 		}
+		free(tmp);
 		return (1);
 	}
 	while (index >= 1)
